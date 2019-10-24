64d82d23d14ce9d5db9da5c1224f9b77938dc2c9
Hannah Rotgeri <hannah.rotgeri@tu-dortmund.de>
2019-10-24 12:52:39 +0200

    Korrektur Blatt2

--- Blatt 2/Blatt2_Aufgabe3_Feline,HannahKorrektur.py
+++ Blatt 2/Blatt2_Aufgabe3_Feline,HannahKorrektur.py
@@ -13,14 +13,14 @@ energy2 = np.linspace(511e-6, 6, 100 )
 
 #Berechnung der invarianten Masse
 
-mass =  np.sqrt( 2 * (511e-6)**2 + 2 * np.outer(energy1, energy2) + np.outer(np.sqrt(energy1**2 - (511e-6)**2), np.sqrt(energy2**2 - (511e-6)**2)))
+mass =  np.sqrt( 2 * (511e-6)**2 + 2 * np.outer(energy1, energy2) + 2 * np.outer(np.sqrt(energy1**2 - (511e-6)**2), np.sqrt(energy2**2 - (511e-6)**2)))
 
 #Aufgabenteil b
 
 plt.plot(energy1, mass[0])
 plt.xlabel(r"Teilchenenergie / GeV")
 plt.ylabel(r"invariante Masse / GeV/c²")
-plt.savefig("Blatt2_Aufgabe3b_Feline,Hannah.pdf")
+plt.savefig("Blatt2_Aufgabe3b_Feline,HannahKorrektur.pdf")
 
 #man sieht den parabolischen verlauf der invarianten masse
 
@@ -29,11 +29,12 @@ plt.savefig("Blatt2_Aufgabe3b_Feline,Hannah.pdf")
 
 fig = plt.figure()
 ax = Axes3D(fig)
-ax.plot_surface(energy1, energy2, mass, rstride=1, cstride=1, cmap=cm.viridis)
+x, y = np.meshgrid(energy1, energy2,)
+ax.plot_surface(x, y, mass, rstride=1, cstride=1, cmap=cm.viridis)
 plt.xlabel(r"Energie Teilchen 1 / GeV")
 plt.ylabel(r"Energie Teilchen 2 / GeV")
 ax.set_zlabel(r"invariante Masse / GeV/c²")
-plt.savefig("Blatt2_Aufgabe3c_Feline,Hannah.pdf")
+plt.savefig("Blatt2_Aufgabe3c_Feline,HannahKorrektur.pdf")
 
 #auch hier ein parabolischer verlauf der invarianten masse
 
